6db7d47f68a0f14a472676cdf3337ac744a6e1d7
h594631 <594631@stud.hvl.no>
2020-10-10 17:23:07 +0200

    Oppgave 5, framgang på A

--- src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
@@ -48,8 +48,13 @@ public class ShowProfile extends EasyGraphics {
 		int x = MARGIN,y;
 
 		// TODO - START
-
-		throw new UnsupportedOperationException(TODO.method());
+		// drawLine(int startX, int startY, int endX, int endY)
+		for (GPSPoint gpspoint : gpspoints) {
+			int tempYbase = ybase - (int)gpspoint.getElevation() > 0 ? ybase - (int)gpspoint.getElevation() : 0;
+			drawLine(MARGIN, ybase, x+1, tempYbase);
+			x += 3;
+		}
+		//throw new UnsupportedOperationException(TODO.method());
 	
 		// TODO - SLUTT
 	}
